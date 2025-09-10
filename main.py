ad57ae6045b5366907f8a596a9425ef65ab2ebbd
Tyrese <tairisu.stewart@gmail.com>
2025-09-10 10:32:07 -0700

    moved the password request down to the next if statement to prompt the user to input their password after the correct username is given

--- main.py
+++ main.py
@@ -32,11 +32,11 @@ users = {
 while attempts < max_attempts: # a loop that iterates until it reaches the max attempts
 
     username = input("Enter username: ")
-    password = getpass.getpass("Enter password: ")
 
-    if username in users: # checks to see if username exists in the directory
+    if username in users: # checks to see if username exists in the directory: if --true-- then ...
+        password = getpass.getpass("Enter password: ")
 
-        if password == users[username]: # checks to see the password entered matches up with password in the directory associated with key-valued pair
+        if password == users[username]: # checks to see the password entered matches up with password in the directory associated with key-valued pair: if --true-- then
 
             print("Login Success")
             break # exit the loop once the login is successful 
