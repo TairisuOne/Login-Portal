d058fa2cbf2454ab3a0f15881b6e9d9e78317b20
Tyrese <tairisu.stewart@gmail.com>
2025-09-10 10:20:58 -0700

    removed the hard-coded username and password for a dirctory styled login system

--- main.py
+++ main.py
@@ -17,22 +17,32 @@ max_attempts = 3
 attempts = 0
 
 
-# hard-coded username and password
-correct_username = "Tyrese"
-correct_password = "123456"
+# hard-coded username and password: test
+"""correct_username = "Tyrese"
+correct_password = "123456"""
 
-
+#  the following is a directory that stores hard-coded username and password in key-valued pairs
+# key --> usernames : values --> passwords
+users = {
+    "Tyrese": "123456",
+    "Alice": "password123",
+    "Thai": "Python12_Cat"
+}
 
 while attempts < max_attempts: # a loop that iterates until it reaches the max attempts
 
     username = input("Enter username: ")
     password = getpass.getpass("Enter password: ")
 
-    if username == correct_username and password == correct_password:
-        print("Login Successful")
+    if username in users: # checks to see if username exists in the directory
 
-        break # exit the loop once the login is successful 
+        if password == users[username]: # checks to see the password entered matches up with password in the directory associated with key-valued pair
 
+            print("Login Success")
+            break # exit the loop once the login is successful 
+        
+        else: 
+            print("Incorrect Creditentials")
     else:
         attempts += 1 # increments the number of attempts whenever the user inputs incorrect username or password
         remaining_attempts = max_attempts - attempts
