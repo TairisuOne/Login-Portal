548c189b3346049e5df01bd79511b04308d897a4
Tyrese <tairisu.stewart@gmail.com>
2025-09-14 15:49:56 -0700

    added consistency and improvements to code: cleaner and removed "break" blocks

--- main.py
+++ main.py
@@ -29,48 +29,69 @@ users = {
     "Thai": "Python12_Cat"
 }
 
+while True:
+    action = input("Do you want to login or register? (login/register): ").lower() # new variable that asks if the user wants to LOGIN or Register
 
-action = input("Do you want to login or register? (login/register): ").lower() # new variable that asks if the user wants to LOGIN or Register
+    if action == "login": # runs if the condition is TRUE
+        logged_in = True
 
-if action == "login": # runs if the condition is TRUE
+        while attempts < max_attempts and logged_in: # a loop that iterates until it reaches the max attempts
 
-    while attempts < max_attempts: # a loop that iterates until it reaches the max attempts
+            username = input("Enter username: ").lower()
 
-        username = input("Enter username: ")
+            if username in users: # checks to see if username exists in the directory: if --true-- then ...
+                password = getpass.getpass("Enter password: ")
 
-        if username in users: # checks to see if username exists in the directory: if --true-- then ...
-            password = getpass.getpass("Enter password: ")
+                if password == users[username]: # checks to see the password entered matches up with password in the directory associated with key-valued pair: if --true-- then
 
-            if password == users[username]: # checks to see the password entered matches up with password in the directory associated with key-valued pair: if --true-- then
+                    print("Login Success")
+                     # exit the loop once the login is successful 
+                    
+                    while logged_in:
+                        menu = input("Account Menu - Logout/Delete Account: ").strip().lower()
 
-                print("Login Success")
-                break # exit the loop once the login is successful 
+                        if menu == "logout":
+                            print(f"{username} signed out.", end="\n")
+
+                            logged_in = False
+
+                        elif menu == "delete":
+                            account_deletion = input("Enter your username to delete: ").strip().lower()
+
+                            if account_deletion in users:
+                                password = getpass.getpass("Enter password to Delete: ")
+
+                                if password == users[account_deletion]:
+
+                                    del users[account_deletion]
+
+                                    print("Account has been deleted")
+                        
+                                    logged_in = False
+                else:
+                    attempts += 1 # increments the number of attempts whenever the user inputs incorrect username or password
+                    remaining_attempts = max_attempts - attempts
+                    print(f"Incorrect credentials. Remaining Attempts {remaining_attempts}")
+                    
+            if username not in users:
+                print("Username not found. Please try again")
+        break
+                
+    elif action == "register":
+
+        while True: # loops until gives a username that's not in use.
+            new_username = input("Choose a username: ").lower()
+
+            if new_username in users:
+                print("Username already in use. Choose another one")
             
             else:
-                attempts += 1 # increments the number of attempts whenever the user inputs incorrect username or password
-                remaining_attempts = max_attempts - attempts
-                print(f"Incorrect credentials. Remaining Attempts {remaining_attempts}")
-                
-        if username not in users:
-            print("Username not found. Please try again")
+                new_password = getpass.getpass("Enter a password: ")
 
-            
-elif action == "register":
+                users[new_username] = new_password
 
-    while True: # loops until gives a username that's not in use.
-        new_username = input("Choose a username: ")
-
-        if new_username in users:
-            print("Username already in use. Choose another one")
-        
-        else:
-            new_password = getpass.getpass("Enter a password: ")
-
-            users[new_username] = new_password
-
-            print("Registration Successful!")
-            break
-
+                print("Registration Successful!")
+                break
 
 # after exiting the following if statement is ran
 if attempts == max_attempts:
